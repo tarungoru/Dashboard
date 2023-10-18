6f7230e64e2258c60c9827a600549724a35c421f
tarun <107760613+tarungoru@users.noreply.github.com>
2023-10-18 15:40:30 +0530

    Add files via upload

--- MyDashboard/src/Banner.jsx
+++ MyDashboard/src/Banner.jsx
@@ -1,34 +1,50 @@
-import nft1 from "assets/img/nfts/NftBanner1.png";
+import React from "react";
+import avatar from "assets/img/avatars/avatar11.png";
+import banner from "assets/img/profile/banner.png";
+import Card from "components/card";
 
-const Banner1 = () => {
+const Banner = () => {
   return (
-    <div
-      className="flex w-full flex-col rounded-[20px] bg-cover px-[30px] py-[30px] md:px-[64px] md:py-[56px]"
-      style={{ backgroundImage: `url(${nft1})` }}
-    >
-      <div className="w-full">
-        <h4 className="mb-[14px] max-w-full text-xl font-bold text-white md:w-[64%] md:text-3xl md:leading-[42px] lg:w-[46%] xl:w-[85%] 2xl:w-[75%] 3xl:w-[52%]">
-          Discover, collect, and sell extraordinary NFTs
-        </h4>
-        <p className="mb-[40px] max-w-full text-base font-medium text-[#E3DAFF] md:w-[64%] lg:w-[40%] xl:w-[72%] 2xl:w-[60%] 3xl:w-[45%]">
-          Enter in this creative world. Discover now the latest NFTs or start
-          creating your own!
-        </p>
-
-        <div className="mt-[36px] flex items-center justify-between gap-4 sm:justify-start 2xl:gap-10">
-          <button className="text-black linear rounded-xl bg-white px-4 py-2 text-center text-base font-medium transition duration-200 hover:!bg-white/80 active:!bg-white/70">
-            Discover now
-          </button>
-          <button
-            href=" "
-            className="text-base font-medium text-lightPrimary hover:text-lightPrimary 2xl:ml-2"
-          >
-            Watch Video
-          </button>
+    <Card extra={"items-center w-full h-full p-[16px] bg-cover"}>
+      {/* Background and profile */}
+      <div
+        className="relative mt-1 flex h-32 w-full justify-center rounded-xl bg-cover"
+        style={{ backgroundImage: `url(${banner})` }}
+      >
+        <div className="absolute -bottom-12 flex h-[87px] w-[87px] items-center justify-center rounded-full border-[4px] border-white bg-pink-400 dark:!border-navy-700">
+          <img className="h-full w-full rounded-full" src={avatar} alt="" />
         </div>
       </div>
-    </div>
+
+      {/* Name and position */}
+      <div className="mt-16 flex flex-col items-center">
+        <h4 className="text-xl font-bold text-navy-700 dark:text-white">
+          Adela Parkson
+        </h4>
+        <p className="text-base font-normal text-gray-600">Product Manager</p>
+      </div>
+
+      {/* Post followers */}
+      <div className="mt-6 mb-3 flex gap-4 md:!gap-14">
+        <div className="flex flex-col items-center justify-center">
+          <p className="text-2xl font-bold text-navy-700 dark:text-white">17</p>
+          <p className="text-sm font-normal text-gray-600">Posts</p>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <p className="text-2xl font-bold text-navy-700 dark:text-white">
+            9.7K
+          </p>
+          <p className="text-sm font-normal text-gray-600">Followers</p>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <p className="text-2xl font-bold text-navy-700 dark:text-white">
+            434
+          </p>
+          <p className="text-sm font-normal text-gray-600">Following</p>
+        </div>
+      </div>
+    </Card>
   );
 };
 
-export default Banner1;
+export default Banner;
